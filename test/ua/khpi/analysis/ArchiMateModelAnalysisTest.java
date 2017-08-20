de6dd487cee5f23a603a10208eb51c89b0cf9654
Andrii Kopp <kopp93@gmail.com>
2017-08-21 00:19:32 +0300

    Add element centrality test

--- test/ua/khpi/analysis/ArchiMateModelAnalysisTest.java
+++ test/ua/khpi/analysis/ArchiMateModelAnalysisTest.java
@@ -1,11 +1,13 @@
 package ua.khpi.analysis;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.apache.jena.rdf.model.Model;
 import org.junit.Before;
 import org.junit.Test;
 
+import ua.khpi.analysis.beans.ArchiMateElement;
 import ua.khpi.util.ArchiMateModelTranslator;
 import ua.khpi.util.ArchiMateModelTranslatorTest;
 
@@ -39,5 +41,15 @@ public class ArchiMateModelAnalysisTest {
 		double expectedDensity = 0.02;
 
 		assertEquals("Density values doesn't match", expectedDensity, archiMateModelDensity, 10E-2);
+	}
+
+	@Test
+	public void testCalculateArchiMateElementsCentrality() {
+		archiMateModelAnalysis.calculateArchiMateElementsCentrality();
+
+		for (ArchiMateElement archiMateElement : archiMateModelAnalysis.getArchiMateModelElements()) {
+			assertTrue(archiMateElement.getElementCentrality() >= 0);
+			assertTrue(archiMateElement.getElementCentrality() <= 1);
+		}
 	}
 }
