888ee99497e84e7de6dc640cd477b72e727510e4
Andrii Kopp <kopp93@gmail.com>
2017-08-20 22:39:17 +0300

    Add analysis class

---
+++ src/ua/khpi/analysis/ArchiMateModelAnalysis.java
@@ -0,0 +1,47 @@
+package ua.khpi.analysis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.ResIterator;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.StmtIterator;
+
+import ua.khpi.analysis.beans.ArchiMateElement;
+
+public class ArchiMateModelAnalysis {
+
+	private Model archiMateModel;
+
+	public ArchiMateModelAnalysis(Model graph) {
+		super();
+		this.archiMateModel = graph;
+	}
+
+	public List<ArchiMateElement> getArchiMateModelElements() {
+		List<ArchiMateElement> archiMateElements = new ArrayList<ArchiMateElement>();
+
+		for (ResIterator iterator = archiMateModel.listSubjects(); iterator.hasNext();) {
+			Resource resource = iterator.nextResource();
+			int propertiesNumber = 0;
+
+			for (StmtIterator j = resource.listProperties(); j.hasNext();) {
+				j.nextStatement();
+				propertiesNumber++;
+			}
+
+			archiMateElements.add(new ArchiMateElement(resource.getLocalName(), propertiesNumber));
+		}
+
+		return archiMateElements;
+	}
+
+	public Model getArchiMateModel() {
+		return archiMateModel;
+	}
+
+	public void setArchiMateModel(Model graph) {
+		this.archiMateModel = graph;
+	}
+}
