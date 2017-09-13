1db9aaf1583eefb0672b9fe2826860d719ab595e
Andrii Kopp <kopp93@gmail.com>
2017-09-13 17:13:30 +0300

    Add MapReduce implementation.

---
+++ src/ua/khpi/analysis/ArchiMateMapReduceModelAnalysis.java
@@ -0,0 +1,52 @@
+package ua.khpi.analysis;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.StmtIterator;
+
+import ua.khpi.analysis.beans.ArchiMateElement;
+
+public class ArchiMateMapReduceModelAnalysis extends ArchiMateModelAnalysis {
+
+	public ArchiMateMapReduceModelAnalysis(Model graph) {
+		super(graph);
+	}
+
+	@Override
+	public List<ArchiMateElement> getArchiMateModelElements() {
+		if (getArchiMateElements() == null) {
+			setArchiMateElements(new ArrayList<ArchiMateElement>());
+
+			map();
+			reduce();
+		}
+
+		return getArchiMateElements();
+	}
+
+	private void map() {
+		for (StmtIterator i = getArchiMateModel().listStatements(); i.hasNext();) {
+			getArchiMateElements().add(new ArchiMateElement(i.next().getSubject().getLocalName(), 1));
+		}
+	}
+
+	private void reduce() {
+		List<ArchiMateElement> result = new ArrayList<ArchiMateElement>();
+
+		for (ArchiMateElement element : getArchiMateElements()) {
+			int index = result.indexOf(element);
+
+			if (index == -1) {
+				result.add(element);
+			} else {
+				int value = result.get(index).getElementPropertiesNumber();
+
+				result.get(index).setElementPropertiesNumber(value + 1);
+			}
+		}
+
+		setArchiMateElements(result);
+	}
+}
